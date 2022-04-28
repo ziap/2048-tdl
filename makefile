da15a0f418615829989259f815c6b54642a748f6
Zap <67962871+ziap@users.noreply.github.com>
2022-04-28 22:08:37 +0700

    Add multithread to agent and update makefile

--- makefile
+++ makefile
@@ -1,9 +1,10 @@
 STRUCTURE?=nw4x6
 ENABLE_TC?=true
+ENABLE_GUI?=true
 CXX?=g++
 
 WEBVIEW_DEPS=`pkg-config --cflags --libs gtk+-3.0 webkit2gtk-4.0`
-OPTIONS=-DSTRUCTURE=$(STRUCTURE) -DENABLE_TC=$(ENABLE_TC) -O3 -mtune=native -std=c++17 -DFILE_NAME=\"$(STRUCTURE)/weights.bin\" -mbmi2
+OPTIONS=-DSTRUCTURE=$(STRUCTURE) -DENABLE_TC=$(ENABLE_TC) -DGUI=$(ENABLE_GUI) -O3 -mtune=native -std=c++17 -DFILE_NAME=\"$(STRUCTURE)/weights.bin\" -mbmi2 -pthread
 all:
 	$(CXX) src/train.cpp -o train $(OPTIONS)
 	$(CXX) src/agent.cpp $(WEBVIEW_DEPS) -o agent $(OPTIONS)
--- src/agent.cpp
+++ src/agent.cpp
@@ -2,12 +2,20 @@
 
 #include <algorithm>
 #include <cstdlib>
+#include <thread>
 
 #include "board.h"
 #include "html.h"
 #include "search.h"
 #include "seed.h"
+
+#ifndef GUI
+#define GUI true
+#endif
+
+#if GUI
 #include "webview/webview.h"
+#endif
 
 #ifndef STRUCTURE
 #define STRUCTURE nw4x6
@@ -17,10 +25,12 @@
 #define FILE_NAME "weights.bin"
 #endif
 
+std::chrono::_V2::high_resolution_clock::time_point start;
 bool showboard = false;
 int games = 1;
+float moves = 0;
+int last_done = 0;
 bool gui = false;
-std::vector<double> speeds;
 std::vector<double> scores;
 int rate[16] = {0};
 
@@ -31,9 +41,11 @@ void ShowStat(int n) {
     if (!showboard) std::cout << "\033[u";
     std::cout << std::fixed << std::setprecision(2);
     std::cout << "progress: " << n << '/' << games;
-    std::cout << "\n\033[Kaverage score: " << std::accumulate(scores.begin(), scores.end(), 0.0) / double(n) << '\n';
+    std::cout << "\n\033[Kaverage score: " << std::accumulate(scores.begin(), scores.end(), 0.0) / scores.size() << '\n';
     std::cout << "\033[Kmaximum score: " << *std::max_element(scores.begin(), scores.end()) << '\n';
-    std::cout << "\033[Kaverage speed: " << std::accumulate(speeds.begin(), speeds.end(), 0.0) / (double)n << " moves per second\n";
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+    std::cout << "\033[Kspeed: " << moves * 1e9 / (float)duration.count() << " moves per second\n";
     auto accu = 0;
     for (auto i = 15; i > 0; i--) {
         if (!rate[i]) continue;
@@ -42,16 +54,43 @@ void ShowStat(int n) {
     }
 }
 
+void game(int index) {
+    auto this_moves = 0;
+    board_t board = AddTile(AddTile(0));
+    auto score = 0;
+    for (;;) {
+        if (showboard) {
+            std::cout << "\033[u";
+            PrintBoard(board);
+        }
+        auto dir = search(board);
+        if (dir < 0) break;
+        this_moves++;
+        score += move.Score(board, dir);
+        board = AddTile(move(board, dir));
+    }
+    scores.push_back(score);
+    rate[MaxRank(board)]++;
+    moves += this_moves;
+    if (index > last_done) {
+        ShowStat(index);
+        last_done = index;
+    }
+}
+
 int main(int argc, char* argv[]) {
     search.Load(FILE_NAME);
     auto c = -1;
-    while ((c = getopt(argc, argv, "d:e:sg")) != -1) switch (c) {
+    auto thread_count = 1;
+    while ((c = getopt(argc, argv, "d:e:tsg")) != -1) switch (c) {
             case 'd': search.min_depth = atoi(optarg); break;
             case 'e': games = atoi(optarg); break;
             case 's': showboard = true; break;
             case 'g': gui = true; break;
+            case 't': thread_count = std::thread::hardware_concurrency(); break;
         }
     if (gui) {
+#if GUI
         auto fn = [&](std::string s) -> std::string {
             board_t b = 0;
             for (int i = 0; i < 4; i++) b = ((b << 16) | std::stoull(webview::json_parse(s, "", i)));
@@ -67,6 +106,9 @@ int main(int argc, char* argv[]) {
         w.set_title("2048 Agent");
         w.set_html(html);
         w.run();
+#else
+        std::cout << "GUI not supported\n";
+#endif
         return 0;
     }
     std::cout << "\x1B[2J\x1B[H";
@@ -76,28 +118,22 @@ int main(int argc, char* argv[]) {
     std::cout << "\033[s";
 
     // Play N games
-    for (auto n = 1; n <= games; n++) {
-        auto moves = 0;
-        auto start = std::chrono::high_resolution_clock::now();
-        board_t board = AddTile(AddTile(0));
-        auto score = 0;
-        for (;;) {
-            if (showboard) {
-                std::cout << "\033[u";
-                PrintBoard(board);
-            }
-            auto dir = search(board);
-            if (dir < 0) break;
-            moves++;
-            score += move.Score(board, dir);
-            board = AddTile(move(board, dir));
+
+    auto n = 1;
+
+    auto pool = std::vector<std::thread>(thread_count);
+
+    auto job = [&]() {
+        while (n <= games) {
+            game(n++);
         }
-        auto end = std::chrono::high_resolution_clock::now();
-        speeds.push_back((double)moves * 1e9 / (double)std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count());
-        scores.push_back(score);
-        rate[MaxRank(board)]++;
-        ShowStat(n);
-    }
+    };
+
+    start = std::chrono::high_resolution_clock::now();
+
+    for (auto& thread : pool) thread = std::thread(job);
+
+    for (auto& thread : pool) thread.join();
     std::cout << '\n';
     return 0;
 }
